1888db7507dfffbe5787b010182417fa5f84b4a8
DenielK <denielkruusman@gmail.com>
2025-11-04 17:13:56 +0200

    Refactor WeatherForecastServices for API key handling

--- ShopTARgv24/ShopTARgv24.ApplicationServices/Services/WeatherForecastServices.cs
+++ ShopTARgv24/ShopTARgv24.ApplicationServices/Services/WeatherForecastServices.cs
@@ -1,45 +1,50 @@
 ﻿using Nancy.Json;
 using ShopTARgv24.Core.Dto;
 using ShopTARgv24.Core.Dto.WeatherWebClientDto;
-using System.Text.Json;
-using System.Net;
 using ShopTARgv24.Core.ServiceInterface;
+using System.Net;
+using System.Text.Json;
 
 
 namespace ShopTARgv24.ApplicationServices.Services
 {
     public class WeatherForecastServices : IWeatherForecastServices
     {
+
         public async Task<AccuLocationWeatherResultDto> AccuWeatherResult(AccuLocationWeatherResultDto dto)
         {
-            string accuApiKey = "zpka_7537561486eb41a49e23a6b9d7957633_53bc9c70";
-            string baseUrl = "http://dataservice.accuweather.com/forecasts/v1/daily/1day";
+            //https://developer.accuweather.com/core-weather/text-search?lang=shell#city-search
+
+            string accuApiKey = ""; //zpka_0c86f3fafa9147e58813fa06b647f221_9b9fd9d9
+            string baseUrl = "http://dataservice.accuweather.com/forecasts/v1/daily/1day/";
 
             using (var httpClient = new HttpClient())
             {
                 httpClient.BaseAddress = new Uri(baseUrl);
                 httpClient.DefaultRequestHeaders.Accept.Clear();
                 httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-                var response = await httpClient.GetAsync($"{127964}?apikey ={accuApiKey}&details=true");
-                //if(response.IsSuccessStatusCode)
-                //{
+                //127964
+                var response = await httpClient.GetAsync($"{127964}?apikey={accuApiKey}&details=true");
+                if (response.IsSuccessStatusCode)
+                {
                     var jsonResponse = await response.Content.ReadAsStringAsync();
                     var weatherData = JsonSerializer.Deserialize<AccuLocationRootDto>(jsonResponse);
-
+                    //return weatherData;
 
                     dto.EndDate = weatherData.Headline.EndDate;
                     dto.Text = weatherData.Headline.Text;
                     dto.TempMaxValue = weatherData.DailyForecasts[0].Temperature.Maximum.Value;
+                }
+                else
+                {
+                    // Handle error response
+                    throw new Exception("Error fetching weather data");
+                }
 
-                //}
-                //else
-                //{
-                throw new Exception("Error fetching weather data");
-                //}
                 return dto;
             }
-
         }
+
 
         public async Task<AccuLocationWeatherResultDto> AccuWeatherResultWebClient(AccuLocationWeatherResultDto dto)
         {
