451f2ccd4135117741e17da93813ed48ccfbb29e
DenielK <denielkruusman@gmail.com>
2025-11-13 23:22:30 +0200

    Delete PageWithGames/PageWithGames directory

--- PageWithGames/PageWithGames/GlobalXmlns.cs
+++
@@ -1,2 +0,0 @@
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "PageWithGames")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "PageWithGames.Pages")]
